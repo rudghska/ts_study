4fc40a78c2607b280fa49e02332b87371177a04a
rudghska <rudghska@gmail.com>
2022-03-28 22:05:38 +0900

    함수에서 타입 사용2

--- 1-TYPE/1-2-funciton.ts
+++ 1-TYPE/1-2-funciton.ts
@@ -17,9 +17,34 @@
   }
 
   //Typescript
+  // Promise 사용하려면 tsconfig.json lib -> es2015 이상으로
   function tsFetchNum(id: string): Promise<number> {
     return new Promise((resolve, reject) => {
       return resolve(100);
     });
   }
+
+  //Optional parameter
+
+  function printName(fName: string, lName?: string) {
+    console.log(fName);
+    console.log(lName);
+  }
+
+  printName('n', 'kh');
+  printName('n');
+
+  //Default parameter
+  function printMsg(msg: string = 'Default Message') {
+    console.log(msg);
+  }
+
+  printMsg();
+
+  //Rest parameter
+  function addNums(...nums: number[]): number {
+    return nums.reduce((a, b) => a + b, 0);
+  }
+
+  addNums(1, 2, 3, 4, 5);
 }
