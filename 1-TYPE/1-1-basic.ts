17a7479f87e1a55a3b1ea39bcbeb2e50309e4ac7
rudghska <rudghska@gmail.com>
2022-03-28 21:32:40 +0900

    기본 타입 정리1

---
+++ 1-TYPE/1-1-basic.ts
@@ -0,0 +1,13 @@
+{
+  // Javascript
+  // Primitive : number, string, boolean, symbol, undefined, null, bingint
+  // Object : function , array
+
+  //Typescript
+
+  //number
+
+  const num: number = 123;
+
+  //string
+}
